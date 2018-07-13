ac9819bcad9c648a2ea7cdecb36397c589cd93cf
davidaureliosantos <davidaureliosantos@yahoo.com.br>
2018-07-12 22:36:02 -0300

    Agora esta funcionando

--- app/src/main/java/digitalhouse/com/botaodeacao/MainActivity.java
+++ app/src/main/java/digitalhouse/com/botaodeacao/MainActivity.java
@@ -1,6 +1,7 @@
 package digitalhouse.com.botaodeacao;
 
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,18 +9,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    CoordinatorLayout cordinator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    cordinator = (CoordinatorLayout) findViewById(R.id.cordinator);
+
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Snackbar snackbar = Snackbar.make(coordinatorLayout, "www.journaldev.com", Snackbar.LENGTH_LONG);
+        Snackbar snackbar = Snackbar.make(cordinator, "www.journaldev.com", Snackbar.LENGTH_LONG);
         snackbar.show();
     }
 
